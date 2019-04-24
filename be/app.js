230d6d64e8b3d22d6bf3e6d6092fc44a72064353
yu-jeong so <liliouu@naver.com>
2019-04-24 11:28:46 +0900

    test

--- be/app.js
+++ be/app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, '../', 'fe', 'dist')));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
-});
+}); 
 
 // error handler
 app.use(function(err, req, res, next) {
