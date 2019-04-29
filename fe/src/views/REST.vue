9bc8e3fa7269e9148232920ef9d8247b08e349ef
yu-jeong so <liliouu@naver.com>
2019-04-29 10:32:11 +0900

    0429 1교시

--- fe/src/views/REST.vue
+++ fe/src/views/REST.vue
@@ -1,9 +1,11 @@
 <template>
   <v-container grid-list-md text-xs-center>
     <v-layout row wrap>
+      <v-btn  absolute dark fab bottom right color="pink" @click="mdUp">
+        <v-icon>add</v-icon>
+      </v-btn>
       <v-flex xs12 sm3>
         <v-card>
-
           <v-card-title primary-title>
             <div>
               <h3 class="headline mb-0">get</h3>
@@ -41,7 +43,6 @@
       </v-flex>
       <v-flex xs12 sm3>
         <v-card>
-
           <v-card-title primary-title>
             <div>
               <h3 class="headline mb-0">put</h3>
@@ -59,7 +60,6 @@
       </v-flex>
       <v-flex xs12 sm3>
         <v-card>
-
           <v-card-title primary-title>
             <div>
               <h3 class="headline mb-0">del</h3>
@@ -75,7 +75,51 @@
           </v-card-actions>
         </v-card>
       </v-flex>
+      <v-dialog v-model="dialog" persistent max-width="600px">
+      <template v-slot:activator="{ on }">
+        <v-btn color="primary" dark v-on="on">Open Dialog</v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">User Profile</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-layout wrap>
+              <v-flex xs12 sm6 md4>
+                <v-text-field label="Legal first name*" required v-model="userName"></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6>
+                <v-select
+                  :items="userAges"
+                  v-model="userAge"
+                  label="Age*"
+                  required
+                ></v-select>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" flat @click="dialog = false">Close</v-btn>
+          <v-btn color="blue darken-1" flat @click="postUser">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     </v-layout>
+    <v-snackbar
+      v-model="snackbar"
+    >
+      {{ sbMsg }}
+      <v-btn
+        color="pink"
+        flat
+        @click="snackbar = false"
+      >
+        Close
+      </v-btn>
+    </v-snackbar>
   </v-container>
 </template>
 <script>
@@ -87,10 +131,17 @@ export default {
       getMd: '',
       postMd: '',
       putMd: '',
-      delMd: ''
+      delMd: '',
+      dialog: false,
+      userAges: [],
+      userAge: 0,
+      userName: '',
+      snackbar: false,
+      sbMsg: ''
     }
   },
   mounted () {
+    for (let i = 10; i < 40; i++) this.userAges.push(i)
   },
   methods: {
     getReq () {
@@ -107,7 +158,8 @@ export default {
 
     postReq () {
       axios.post('http://localhost:3000/api/user', {
-        name:'마', age:12
+        name: this.userName,
+        age: this.userAge
       })
         .then((r) => {
           this.postMd = JSON.stringify(r.data)
@@ -137,6 +189,31 @@ export default {
         .catch((e) => {
           console.error(e.message)
         })
+    },
+
+    mdUp () {
+      console.log('mdUp')
+    },
+
+    postUser () {
+      console.log(this.userName, this.userAge)
+      this.dialog = false
+      this.pop(this.userName)
+      axios.post('http://localhost:3000/api/user', {
+        name: this.userName,
+        age: this.userAge
+      })
+        .then((r) => {
+          this.pop('사용자 등록 완료')
+        })
+        .catch((e) => {
+          this.pop(e.message)
+        })
+    },
+
+    pop (msg) {
+      this.snackbar = true
+      this.sbMsg = msg
     }
   }
 }
